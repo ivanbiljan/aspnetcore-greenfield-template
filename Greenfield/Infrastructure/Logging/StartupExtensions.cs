514619765d1c38166dbe78c4bf84ba0eebd62025
Ivan Biljan <ssbiljan@gmail.com>
2024-03-18 09:42:29 +0100

    Add Serilog configuration

---
+++ Greenfield/Infrastructure/Logging/StartupExtensions.cs
@@ -0,0 +1,61 @@
+﻿using Destructurama;
+using Serilog;
+using Serilog.Events;
+using Serilog.Exceptions;
+using Serilog.Exceptions.Core;
+using Serilog.Exceptions.Refit.Destructurers;
+
+namespace Greenfield.Infrastructure.Logging;
+
+public static class StartupExtensions
+{
+    /// <summary>
+    ///     Sets Serilog as the default logging provider and configures its enrichers and sinks.
+    /// </summary>
+    /// <param name="builder">The <see cref="IHostBuilder" /> to configure.</param>
+    /// <returns>The modified <paramref name="builder" />.</returns>
+    public static IHostBuilder ConfigureSerilog(this IHostBuilder builder)
+    {
+        return builder.UseSerilog(
+            (_, loggerConfiguration) =>
+            {
+                loggerConfiguration.MinimumLevel.Information();
+                loggerConfiguration.MinimumLevel.Override("Microsoft", LogEventLevel.Warning);
+                loggerConfiguration.MinimumLevel.Override("Microsoft.Hosting.Lifetime", LogEventLevel.Information);
+                loggerConfiguration.MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Information);
+
+                loggerConfiguration.Enrich.FromLogContext();
+                loggerConfiguration.Enrich.WithMachineName();
+                loggerConfiguration.Enrich.WithEnvironmentName();
+                loggerConfiguration.Enrich.WithExceptionDetails(
+                    new DestructuringOptionsBuilder().WithDestructurers(
+                        new[]
+                        {
+                            new ApiExceptionDestructurer()
+                        }));
+
+                loggerConfiguration.Destructure.UsingAttributes();
+
+                loggerConfiguration.WriteTo.Console();
+            });
+    }
+
+    /// <summary>
+    ///     Adds Serilog's HTTP request logging middleware to the pipeline. Enriches requests with the user's identity and
+    ///     RemoteIpAddress. Request and response bodies are not logged.
+    /// </summary>
+    /// <param name="app">The <see cref="IApplicationBuilder" />.</param>
+    /// <returns>The modified <paramref name="app" />.</returns>
+    public static IApplicationBuilder UseSerilogHttpLogging(this IApplicationBuilder app)
+    {
+        return app.UseSerilogRequestLogging(
+            options =>
+            {
+                options.EnrichDiagnosticContext = static (diagnosticContext, httpContext) =>
+                {
+                    diagnosticContext.Set("User", httpContext.User.Identity?.Name);
+                    diagnosticContext.Set("RemoteIP", httpContext.Connection.RemoteIpAddress);
+                };
+            });
+    }
+}
--- Greenfield/Program.cs
+++ Greenfield/Program.cs
@@ -1,44 +1,33 @@
-var builder = WebApplication.CreateBuilder(args);
+using Serilog;
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
 
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    var builder = WebApplication.CreateBuilder(args);
 
-app.UseHttpsRedirection();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+    var app = builder.Build();
 
-app.MapGet("/weatherforecast", () =>
+    if (app.Environment.IsDevelopment())
     {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast")
-    .WithOpenApi();
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
 
-app.Run();
+    app.UseHttpsRedirection();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+    app.Run();
+}
+catch (Exception ex) when (ex is not HostAbortedException)
 {
-    public int TemperatureF => 32 + (int) (TemperatureC / 0.5556);
+    Log.Fatal(ex, "Unexpected exception during host bootstrapping");
+}
+finally
+{
+    Log.CloseAndFlush();
 }
