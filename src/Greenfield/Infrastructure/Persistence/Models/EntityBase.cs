af947ef458ff5b0e700b87654af72114214a39ea
ivanbiljan <ssbiljan@gmail.com>
2024-03-23 17:24:51 +0100

    Add EntityBase

---
+++ src/Greenfield/Infrastructure/Persistence/Models/EntityBase.cs
@@ -0,0 +1,8 @@
+﻿namespace Greenfield.Infrastructure.Persistence.Models;
+
+public abstract class EntityBase<TKey>
+{
+    public TKey Id { get; init; } = default!;
+
+    public DateTime CreatedOnUtc { get; init; } = DateTime.UtcNow;
+}
